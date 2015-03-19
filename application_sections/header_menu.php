eb5ee515ed8d42cba94033d8e99e324a3e48bc95
erickdav <erickdavidmg@gmail.com>
2015-03-19 11:55:30 -0500

    Erick David Montaño

--- application_sections/header_menu.php
+++ application_sections/header_menu.php
@@ -29,13 +29,13 @@
 						    <!--- Campos de la pagina -->
 						      <ul class="nav navbar-nav">
 
-																<li><a class="opciones-menu" href="<?=  BASE_URL ;?>Empresa/nuestra_empresa">Nuestra Empresa</a></li>
-																<li><a class="opciones-menu" href="<?=  BASE_URL ;?>Empresa/desarrollo_de_productos">Desarrollo De Productos</a></li>
-																<li><a class="opciones-menu" href="<?=  BASE_URL ;?>Empresa/linea_de_productos">Línea De Productos     </a></li>
-																<li><a class="opciones-menu" href="<?=  BASE_URL ;?>Empresa/contro_de_calidad">Control De Calidad     </a></li>
-																<li><a class="opciones-menu" href="<?=  BASE_URL ;?>Empresa/nuestros_servicios">Nuestros Servicios     </a></li>
-																<li><a class="opciones-menu" href="<?=  BASE_URL ;?>Empresa/nuestros_clientes">Nuestros Clientes      </a></li>
-																<li><a class="opciones-menu" href="<?=  BASE_URL ;?>Empresa/contactanos">Contáctenos            </a></li>
+																<li class="uno_balk"><a class="opciones-menu" href="<?=  BASE_URL ;?>Empresa/nuestra_empresa">Nuestra Empresa</a></li>
+																<li class="dos_balk"><a class="opciones-menu" href="<?=  BASE_URL ;?>Empresa/desarrollo_de_productos">Desarrollo De Productos</a></li>
+																<li class="tres_balk"><a class="opciones-menu" href="<?=  BASE_URL ;?>Empresa/linea_de_productos">Línea De Productos     </a></li>
+																<li class="cuatro_balk"><a class="opciones-menu" href="<?=  BASE_URL ;?>Empresa/contro_de_calidad">Control De Calidad     </a></li>
+																<li class="cinco_balk"><a class="opciones-menu" href="<?=  BASE_URL ;?>Empresa/nuestros_servicios">Nuestros Servicios     </a></li>
+																<li class="seis_balk"><a class="opciones-menu" href="<?=  BASE_URL ;?>Empresa/nuestros_clientes">Nuestros Clientes      </a></li>
+																<li class="siete_balk"><a class="opciones-menu" href="<?=  BASE_URL ;?>Empresa/contactanos">Contáctenos            </a></li>
 
 						      </ul>
 						     <!--- Campos de la pagina -->
@@ -46,3 +46,21 @@
 
     </div><!-- Contenedor -->
 </div>
+
+<!--
+					***
+					Estas Clases las utilizamos para dar le elefecto activo a
+					los campos de l menu...en cada hoja de estilos de acuerdo
+					a la pagina se encentra un diseño el cual cumple
+					con la funcion e dar el efecto activo
+    ***
+
+					class ="uno_balk"    = Nuestra Empresa
+					class ="dos_balk"    = Desarrollo de Productos
+					class ="tres_balk    = Linea de Productos
+					class ="cuatro_balk" = Control  de Calidad
+					class ="cinco_balk"  = Nuestros Servicios
+					class ="seis_balk"   = Nuestros Clientes
+					class ="siete_balk"  = Contactanos
+-->
+
